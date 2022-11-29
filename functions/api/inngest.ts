3cb06942e1b9f3367d163d63c6416a57291c0669
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-11-29 11:43:17 +0000

    Use new Inngest function export method for integration testing

--- functions/api/inngest.ts
+++ functions/api/inngest.ts
@@ -1,8 +1,4 @@
-import { createFunction } from "inngest";
 import { serve } from "inngest/cloudflare";
+import fns from "../../inngest";
 
-const foo = createFunction("Foo", "demo/event.sent", () => {
-  console.log("Got a demo event!");
-});
-
-export const onRequest = serve("Cloudflare Pages Example", [foo]);
+export const onRequest = serve("Cloudflare Pages Example", fns);
